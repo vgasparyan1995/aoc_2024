65e5c4baed84676d9fb0556e540090026c5bd456
vgasparyan1995 <vgasparyan1995@gmail.com>
2024-12-02 13:22:25 +0000

    Optimization, simplification. Part2 still has an error, to be inspected.

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,33 +1,27 @@
 use std::io::{stdin, BufRead};
 
-fn is_safe(report: &[i32]) -> bool {
-    let sign = (report[0] - report[1]).signum();
+fn safety_errors(report: &[i32]) -> usize {
+    let sign = (report[0] - report[report.len() - 1]).signum();
     (0..report.len() - 1)
-        .map(|i| (report[i], report[i + 1]))
-        .map(|(a, b)| a - b)
-        .all(|d| d.signum() == sign && d.abs() <= 3 && d.abs() >= 1)
-}
-
-fn is_almost_safe(report: &[i32]) -> bool {
-    is_safe(report)
-        || (0..report.len())
-            .map(|i| {
-                let it = report.iter();
-                it.clone()
-                    .take(i)
-                    .chain(it.clone().skip(i + 1))
-                    .map(|&n| n)
-                    .collect::<Vec<_>>()
-            })
-            .any(|new_report| is_safe(&new_report[..]))
+        .map(|i| report[i] - report[i + 1])
+        .filter(|delta| delta.signum() != sign || delta.abs() > 3 || delta.abs() == 0)
+        .count()
 }
 
 fn part1(input: &Vec<Vec<i32>>) -> usize {
-    input.iter().filter(|report| is_safe(report)).count()
+    input
+        .iter()
+        .map(|report| safety_errors(report))
+        .filter(|&cnt| cnt == 0)
+        .count()
 }
 
 fn part2(input: &Vec<Vec<i32>>) -> usize {
-    input.iter().filter(|report| is_almost_safe(report)).count()
+    input
+        .iter()
+        .map(|report| safety_errors(report))
+        .filter(|&cnt| cnt <= 1)
+        .count()
 }
 
 fn main() {
