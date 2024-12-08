4e0b332d4f90eb5efbb79a62eb3da48fccd9587b
Vanand Gasparyan <vgasparyan1995@gmail.com>
2024-12-08 03:38:01 +0100

    Day 6, part 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -45,8 +45,69 @@ fn part1(mtx: &Mtx) -> Option<usize> {
     Some(positions.len())
 }
 
+fn obstacle_loops(mtx: &mut Mtx, r: i32, c: i32, dr: i32, dc: i32, r_max: i32, c_max: i32) -> bool {
+    if get(mtx, r, c, r_max, c_max) != Some('.') {
+        return false;
+    }
+    let mut loop_found = false;
+    mtx[r as usize][c as usize] = '#';
+    {
+        let (mut r, mut c) = (r - dr, c - dc);
+        let (mut dr, mut dc) = (dr, dc);
+        let mut positions = HashSet::new();
+        loop {
+            if !positions.insert((r, c, dr, dc)) {
+                loop_found = true;
+                break;
+            }
+            while let Some('#') = get(mtx, r + dr, c + dc, r_max, c_max) {
+                (dr, dc) = turn_right((dr, dc));
+            }
+            if let None = get(mtx, r + dr, c + dc, r_max, c_max) {
+                break;
+            }
+            (r, c) = (r + dr, c + dc);
+        }
+    }
+    mtx[r as usize][c as usize] = '.';
+    loop_found
+}
+
+fn part2(mtx: &mut Mtx) -> Option<usize> {
+    let r_max = mtx.len() as i32;
+    let c_max = mtx[0].len() as i32;
+    let (mut r, mut c) = (0..r_max)
+        .cartesian_product(0..c_max)
+        .find(|&(r, c)| get(mtx, r, c, r_max, c_max) == Some('^'))?;
+    let (mut dr, mut dc) = (-1, 0);
+    let mut visited = HashSet::new();
+    let mut obstacles = HashSet::new();
+    loop {
+        visited.insert((r, c));
+        while let Some('#') = get(mtx, r + dr, c + dc, r_max, c_max) {
+            (dr, dc) = turn_right((dr, dc));
+        }
+        if let None = get(mtx, r + dr, c + dc, r_max, c_max) {
+            break;
+        }
+        (r, c) = (r + dr, c + dc);
+        // Don't put obstacles on visited positions, yields false positives.
+        if visited.contains(&(r, c)) {
+            continue;
+        }
+        if obstacles.contains(&(r, c)) {
+            continue;
+        }
+        // Test an obstacle.
+        if obstacle_loops(mtx, r, c, dr, dc, r_max, c_max) {
+            obstacles.insert((r, c));
+        }
+    }
+    Some(obstacles.len())
+}
+
 fn main() {
-    let input = stdin()
+    let mut input = stdin()
         .lock()
         .lines()
         .map(Result::unwrap)
@@ -54,5 +115,5 @@ fn main() {
         .collect::<Mtx>();
 
     println!("Part1: {:?}", part1(&input));
-    // println!("Part2: {}", part2(&input));
+    println!("Part2: {:?}", part2(&mut input));
 }
